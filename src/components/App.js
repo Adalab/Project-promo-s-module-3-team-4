831d7566f1e42be43db12d8ea86dd75e7f774f7e
Cristina-F-R <cristina.faixedas@gmail.com>
2023-03-05 12:57:15 +0100

    variables SCSS

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import '../styles/App.scss';
+import '../styles/main.scss';
 import cover from '../images/cover.jpeg';
 import user from '../images/user.jpeg';
 
@@ -12,7 +12,7 @@ function App() {
           </header>
           <main className="main">
             <section className="preview">
-              <img className="image" src={cover} alt="" />
+              <img className="image" src={cover} alt="cover" />
 
               <section className="autor">
                 <section className="info-project">
@@ -33,7 +33,7 @@ function App() {
                 </section>
 
                 <section className="info-autor">
-                  <img className="image" src={user} alt="" />
+                  <img className="image" src={user} alt="user" />
                   <p className="job">Full Stack Developer</p>
                   <p className="name">Emmelie Björklund</p>
                 </section>
@@ -127,7 +127,7 @@ function App() {
 
               <section className="card">
                 <span className=""> La tarjeta ha sido creada: </span>
-                <a href="" className="" target="_blank" rel="noreferrer"></a>
+                {/* <a href="" className="" target="_blank" rel="noreferrer"></a> */}
               </section>
             </section>
           </main>
